3477e0279cc68a8551bcb196e5ed226d2a7ea79b
Yehia Arja <yehia.al.arja12@gmail.com>
2025-03-23 15:00:43 +0200

    added the pivot table code snippet tag

--- backend/database/migrations/2025_03_23_123326_create_code_snippet_tag_table.php
+++ backend/database/migrations/2025_03_23_123326_create_code_snippet_tag_table.php
@@ -12,8 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('code_snippet_tag', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('code_snippet_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->foreignId('tag_id')
+                ->constrained()
+                ->onDelete('cascade');
+            $table->primary(['code_snippet_id', 'tag_id']);
         });
     }
 
